8a2769767835c4d6991274af9e3cc4d0fea682c1
gwinkless <github@geoff.dj>
2026-06-21 15:24:29 +0100

    set the gpios for the final manufactured board

--- main.h
+++ main.h
@@ -31,18 +31,28 @@
 // Hardware map
 
 // Sam output pins
+// on our pico board, these were 5-9
+/*
 // I'm an idiot for not putting these in order in my PCB, since it breaks the pio version
 // we're also swapping left+right and ctrl+down, because I'm even more of an idiot and put
 // the connector on backwards
 #define SamMouseBit0_PIN 2
-#define SamMouseBit1_PIN 6 
+#define SamMouseBit1_PIN 6
 #define SamMouseBit2_PIN 5
 #define SamMouseBit3_PIN 3
 // MouseBit4 isn't used by the sam mouse, but we want to use it
 // for the joystick to ctrl+up+down+left+right
 #define SamMouseBit4_PIN 7
 // the (inverted) RDM select pin goes next. 22 on the pico board.
-#define RDMSEL_PIN 4
+#define RDMSEL_PIN 4*/
+// these for the final board
+#define SamMouseBit0_PIN 3
+#define SamMouseBit1_PIN 4
+#define SamMouseBit2_PIN 5
+#define SamMouseBit3_PIN 6
+#define SamMouseBit4_PIN 7
+#define RDMSEL_PIN 2
+
 #define SamMousePinsMask ((1<<SamMouseBit0_PIN)|(1<<SamMouseBit1_PIN)|(1<<SamMouseBit2_PIN)|(1<<SamMouseBit3_PIN)|(1<<SamMouseBit4_PIN))
 // Mouse Status LED. 25 on a Pico
 #define STATUS_PIN 8
@@ -51,11 +61,19 @@
 #define PICO_DEFAULT_LED_PIN STATUS_PIN
 
 // joystick pins
-#define JoystickUp_PIN 25
+/*#define JoystickUp_PIN 25
 #define JoystickDown_PIN 26
 #define JoystickLeft_PIN 27
 #define JoystickRight_PIN 28
 #define JoystickFire_PIN 29
+*/
+// these for the final board
+#define JoystickFire_PIN 25
+#define JoystickUp_PIN 26
+#define JoystickDown_PIN 27
+#define JoystickLeft_PIN 28
+#define JoystickRight_PIN 29
+
 
 
 #define JoystickPinsMask ((1<<JoystickUp_PIN)|(1<<JoystickDown_PIN)|(1<<JoystickLeft_PIN)|(1<<JoystickRight_PIN)|(1<<JoystickFire_PIN))
