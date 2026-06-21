926555eeec0d2c126601d66083aabb4ced46ba36
gwinkless <github@geoff.dj>
2026-06-21 10:44:37 +0100

    don't slow down the flash. the flash we use is fine at speed.

--- main.c
+++ main.c
@@ -37,9 +37,9 @@
 // Override default USB pin
 //#define PIO_USB_DP_PIN_DEFAULT 8
 
-// Configure RP2040 for slower flash
-#define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 64
-#define PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H 1
+// don't need to configure RP2040 for slower flash, since our flash is plenty fast enough, thanks
+//#define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 64
+//#define PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H 1
 
 #define VERSION "1.0"
 
